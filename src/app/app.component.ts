aa747d0702d3969df2f377b1a0a6ac9e81c3fcdb
KhanhNguyen206 <nguyen.ngockhanh206@gmail.com>
2019-08-02 11:26:37 +0700

    Angular Countdown Timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-countdown';
+
+  countdownMsg = '';
+  finishCountdown(value) {
+    console.log(value);
+    this.countdownMsg = 'Finished!';
+  }
 }
